a6ba61d67efb1ad46ceaf8bea0cc520dfaa26a06
Zhenya <zhenya@mail.ru>
2022-07-11 20:08:39 +0900

    add Zadacha 0

--- Zadacha0/Program.cs
+++ Zadacha0/Program.cs
@@ -7,14 +7,18 @@
 // -3 -> 9
 // -7 -> 49
 
-int a = 4;
-int b = -3;
-int c = -7;
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int a = 4; // -2.147 млрд до 2.147 млрд
+// int b = -3;
+// int c = -7;
 
-int res1 = a * a;
-int res2 = b * b;
-int res3 = c * c;
+int res1 = num * num;
+// int res2 = b * b;
+// int res3 = c * c;
 
-Console.WriteLine(res1);
-Console.WriteLine(res2);
-Console.WriteLine(res3);
+Console.WriteLine($"Квадрат числа: {num} = {res1}");
+// Console.WriteLine(res2);
+// Console.WriteLine(res3);
+// $ - позволяет нам напечатать значения переменных
