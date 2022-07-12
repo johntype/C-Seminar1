199bd1d558413a0ea1ab9be362b9cae4f27fa804
Zhenya <zhenya@mail.ru>
2022-07-13 08:48:57 +0900

    add zadacha 1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -7,19 +7,59 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
+
+// Console.Clear();
+// Console.Write("Введите целое число: ");
+// int num_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите целое число: ");
+// int num_2 = Convert.ToInt32(Console.ReadLine());
+// int result = num_2 * num_2;
+//     if (result == num_1) 
+//     {
+//         Console.WriteLine("Да");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Нет");
+//     }
+
 // 3. Напишите программу, которая будет выдавать
 // название дня недели по заданному номеру.
 // 3 -> Среда
 // 5 -> Пятница
 
-int a = 25;
-int b = 5;
-int result = b * b;
-    if (result == a) 
+Console.Write("Введите целое число: ");
+int day_week = Convert.ToInt32(Console.ReadLine());
+    if (day_week == 1)
     {
-        Console.WriteLine("Да");
+        Console.WriteLine("Понедельник");
     }
-    else 
+    else if (day_week == 2)
     {
-        Console.WriteLine("Нет");
+        Console.WriteLine("Вторник");
     }
+     else if (day_week == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+     else if (day_week == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+     else if (day_week == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+     else if (day_week == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+     else if (day_week == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+     else 
+    {
+        Console.WriteLine("Ошибка");
+    }
+   
