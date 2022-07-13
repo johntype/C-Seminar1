a0a3d3a364ca103be179a26f06d863e6b10886b4
Zhenya <zhenya@mail.ru>
2022-07-14 08:33:05 +0900

    add fail Task2

--- HomeWork/Task_2/Program.cs
+++ HomeWork/Task_2/Program.cs
@@ -9,7 +9,6 @@ Console.Write("Введите целое число: ");
 int num_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число: ");
 int num_2 = Convert.ToInt32(Console.ReadLine());
-int max = 0;
     if (num_1 < num_2) 
     {
     Console.WriteLine($"Max =  {num_2}" );
